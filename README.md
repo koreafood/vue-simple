3f315dd87b0aa4ebd5353614b1568bde143bae70
koreafood <koreafood@gmail.com>
2020-12-22 09:19:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-zrcujz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-zrcujz)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import _ from "lodash";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  mounted() {
+    console.info("paper mounted");
+    const data = {
+      dims: [
+        "AI Guide_Base",
+        "AI Guide_Simul.",
+        "D/C Rate_Base",
+        "D/C Rate_Simul."
+      ],
+      weeks: 52
+    };
+
+    
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
